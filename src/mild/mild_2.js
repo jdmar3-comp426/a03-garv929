0b46db983e44cb78a01589fa289e043da74dd827
Garv Srivastava <gsrivast@email.unc.edu>
2021-12-01 02:54:49 -0500

    mild 2 commit 6

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -70,6 +70,8 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
+   delete object[key];
+
 
 }
 
