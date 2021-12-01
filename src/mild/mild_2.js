381d62c0d429e5e6ba7896cc38751fe654717376
Garv Srivastava <gsrivast@email.unc.edu>
2021-12-01 02:48:46 -0500

    mild 2 commit 4

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -50,6 +50,9 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
+   if(key in object){
+      object.delete(key);
+   }
 
 }
 
